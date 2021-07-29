9d8f330c9423ab72d096c03c22634d32146ced25
Benjamin <h8bmf@hotmail.co.uk>
2021-07-29 20:39:16 +0100

    Update 29-07-2021

--- ExceptionsDataTypes/ExceptionsApp/Program.cs
+++ ExceptionsDataTypes/ExceptionsApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace ExceptionsApp
 {
@@ -7,6 +8,37 @@ namespace ExceptionsApp
     {
         static void Main(string[] args)
         {
+            var result = 5.50 / 5;
+            var myInt = 5;
+            double myNum = 5;
+            //int anInt = 3; //Explicitly typed
+            //var vInt = 3; //Implicity typed
+            //var v2Int = vInt; // Implicitly typed 
+
+            //var name = "Cathy";
+            //var isClean = true;
+            //var letter = 'P';
+            //var uLongNum = 52uL;
+
+            //List<decimal> prices = new List<decimal>();
+
+            short n1 = -65;
+            ushort n2 = 65;
+            int n3 = -100_100;
+            uint n4 = 100_000;
+            var max = Int32.MaxValue;
+            uint n5 = 4_000_000_000;
+            long n6 = -5_000_000_000;
+            ulong n7 = 5_000_000_000;
+
+            float sum = 0; 
+            for(int i = 0; i < 100_000; i++)
+            {
+                sum += 2 / 5.0f;
+            }
+            Console.WriteLine("2/5 added 100,000 times " + sum);
+            Console.WriteLine("2/5 mulitiplied 100,100 times " + 2/5.0f * 100_000);
+
             string text;
             string filename = "";
             try
